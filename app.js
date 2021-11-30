43a6d4b5ed6f9d77058b1dc87c690d4f7d974a7f
Arturo Grottoli <vamosdavid@gmail.com>
2021-11-30 15:44:14 -0300

    Video

--- app.js
+++ app.js
@@ -1,21 +1,26 @@
- $(function() {
-     $(window).scroll(function() {
-         if ($(this).scrollTop() < 720) {
-             $('#navscroll').addClass("gris");
-         } else {
-             $('#navscroll').removeClass("gris");
-             $('#navscroll').addClass("azul");
-         }
-     });
- });
+//Navbar transparente a Navbar color
 
- $(function() {
-     $(window).scroll(function() {
-         if ($(this).scrollTop() > 720) {
-             $('#navscroll').addClass("azul");
-         } else {
-             $('#navscroll').removeClass("azul");
-             $('#navscroll').addClass("gris");
-         }
-     });
- });
+$(function () {
+    $(window).scroll(function () {
+        if ($(this).scrollTop() < 720) {
+            $('#navscroll').addClass("gris");
+        } else {
+            $('#navscroll').removeClass("gris");
+            $('#navscroll').addClass("azul");
+        }
+    });
+});
+
+$(function () {
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 720) {
+            $('#navscroll').addClass("azul");
+        } else {
+            $('#navscroll').removeClass("azul");
+            $('#navscroll').addClass("gris");
+        }
+    });
+});
+
+
+
