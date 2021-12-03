1122de13b481df9c2a2705a6ed4ae5b6bc230843
Arturo Grottoli <vamosdavid@gmail.com>
2021-12-03 12:08:07 -0300

    Formulario

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-//Navbar transparente a Navbar color
+/*Navbar transparente a Navbar color*/
 
 $(function () {
     $(window).scroll(function () {
@@ -22,5 +22,31 @@ $(function () {
     });
 });
 
+
+/*Funcion Formulario*/
+const $form = document.querySelector('#form')
+$form.addEventListener('submit', handleSubmit)
+
+async function handleSubmit(event) {
+    event.preventDefault()
+    const form = new FormData(this)
+    const response = await fetch(this.action, {
+        method: this.method,
+        body: form,
+        headers: {
+            'Accept': 'application/json'
+        }
+    })
+
+    if (response.ok) {
+        this.reset()
+        swal({
+            title: "Tu mensaje fue enviado!",
+            text: "Gracias por contactarte, nos comunicaremos con vos lo mas pronto posible.",
+            icon: "success",
+        });
+
+    }
+}
 
 
